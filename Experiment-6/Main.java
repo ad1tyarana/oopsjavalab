1bb8f18f671bc12b52a119a5273670894ac7dcd3
ad1tyarana <aditya9260rana@gmail.com>
2025-04-04 15:25:51 +0530

    Create Main.java

---
+++ Experiment-6/Main.java
@@ -0,0 +1,8 @@
+import Balance.Account;  // Importing Account class 
+
+public class Main {
+    public static void main(String[] args) {
+        Account myAccount = new Account(1500.75); // Creating Account object
+        myAccount.Display_Balance();  // Calling the method
+    }
+}
