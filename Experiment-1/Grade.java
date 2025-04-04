4c194db198a848b7485dbf0c1e02afa2ccc6d060
ad1tyarana <aditya9260rana@gmail.com>
2025-04-04 14:51:07 +0530

    Create Grade.java

---
+++ Experiment-1/Grade.java
@@ -0,0 +1,12 @@
+public class Grade {
+    public static void main(String[] args){
+        System.out.println("Roll Number "+" Name                    "+"                   Subjects               ");
+        System.out.println("           "+"                         "+"Physics "+" Chemistry "+" Biology "+" Maths "+" English");
+        System.out.println("10          "+" Dhruv Arora            "+"76      "+" 71        "+" 83      "+" 68.7  "+" 76     ");
+        System.out.println("17          "+" Harman Kaur            "+"82.5    "+" 76        "+" 80      "+" 63    "+" 92.5   ");
+        System.out.println("23          "+" Aanand Sharma          "+"69      "+" 74.7      "+" 73      "+" 60    "+" 77     ");
+        System.out.println("26          "+" Vinay Singh            "+"55.5    "+" 49.5      "+" 64      "+" 65    "+" 70     ");
+        System.out.println("29          "+" Shreya Singh           "+"88      "+" 80        "+" 83      "+" 78.7  "+" 91     ");
+    }
+    
+}
