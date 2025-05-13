f4002292520790f16879d1401cde4b856fd32d06
ad1tyarana <aditya9260rana@gmail.com>
2025-05-13 22:04:56 +0530

    Add files via upload

---
+++ EXPERIMENT-8/Library.java
@@ -0,0 +1,24 @@
+class Library {
+    static class Book {
+        String title;
+        String author;
+        String ISBN;
+
+        Book(String title, String author, String ISBN) {
+            this.title = title;
+            this.author = author;
+            this.ISBN = ISBN;
+        }
+
+        void displayDetails() {
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+            System.out.println("ISBN: " + ISBN);
+        }
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("The white tiger", "Aravind Adiga", "1234567890");
+        book.displayDetails();
+    }
+}
